3ec0a0f0de6cb048fc7ddc8cc663cc74145b59f1
Jackson Barker <jcrb45@hotmail.com>
2024-05-27 14:24:59 -0500

    styling

--- src/components/HourlyForecast.jsx
+++ src/components/HourlyForecast.jsx
@@ -21,26 +21,26 @@ export default function HourlyForecast() {
   const filteredHours = filterHours();
   return (
     <div id="hourly-forecast">
-      <AnimatePresence mode='wait'>
-      <motion.h3
-        key={"hourlyHeader" + selectedDay.time}
-        animate={{ opacity: [0, 1], x: [-100, 0],filter: "blur(0px)", }}
-        initial={{ opacity: [1, 0], x: [0, -100], filter: "blur(5px)"}}
-        exit={{ opacity: [1, 0], x: [0, -100] }}
-        transition={{duration: 0.15}}
-        className="hourly-header"
-      >
-        {Object.keys(selectedDay).length > 0
-          ? formatDate(selectedDay.time)
-          : "Today"}
-      </motion.h3>
+      <AnimatePresence mode="wait">
+        <motion.h3
+          key={"hourlyHeader" + selectedDay.time}
+          animate={{ opacity: [0, 1], filter: "blur(0px)" }}
+          initial={{ opacity: [1, 0], filter: "blur(5px)" }}
+          //exit={{ opacity: [1, 0], x: [0, -100] }}
+          transition={{ duration: 0.15 }}
+          className="hourly-header"
+        >
+          {Object.keys(selectedDay).length > 0
+            ? formatDate(selectedDay.time)
+            : "Today"}
+        </motion.h3>
       </AnimatePresence>
       <AnimatePresence mode="popLayout">
         {hourly && (
           <motion.ul
             key={"hourlyList" + selectedDay.time}
             variants={{
-              visible: { transition: { staggerChildren: 0.05} },
+              visible: { transition: { staggerChildren: 0.05 } },
             }}
             initial="hidden"
             animate="visible"
