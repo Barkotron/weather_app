e9532a828b34c15f4ac238388ac557e8e591b51e
Jackson Barker <jcrb45@hotmail.com>
2024-05-30 16:12:52 -0500

    moved refresh button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,16 @@
+import { useContext } from "react";
+import { WeatherContext } from "../store/WeatherContext";
+import { refreshWeather } from "../util/http";
+
 export default function Header() {
+
+  const weatherCtx = useContext(WeatherContext);
   return (
     <div id="header">
       <header >
         <h1>Weather App</h1>
       </header>
+      <button onClick={() => refreshWeather(weatherCtx)}>Refresh</button>
     </div>
   );
 }
